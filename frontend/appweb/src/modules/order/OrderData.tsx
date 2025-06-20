9f53eaa8e5ca7a46258a7f9cabdbdf4fec3c3083
Walkup19 <169078515+Walkup19@users.noreply.github.com>
2025-06-20 10:01:41 -0600

    menu

--- frontend/appweb/src/modules/order/OrderData.tsx
+++ frontend/appweb/src/modules/order/OrderData.tsx
@@ -2,9 +2,9 @@ const OrderData = () => {
   return (
     <div>
       <h2>Ordenes</h2>
-      {/* Aquí tu contenido */}
+     
     </div>
   )
 }
 
-export default OrderData // ← NECESARIO para que funcione la importación actual
+export default OrderData 
--- frontend/appweb/src/modules/product/ProductoData.tsx
+++ frontend/appweb/src/modules/product/ProductoData.tsx
@@ -2,9 +2,9 @@ const ProductoData = () => {
   return (
     <div>
       <h2>Productos</h2>
-      {/* Aquí tu contenido */}
+      
     </div>
   )
 }
 
-export default ProductoData // ← NECESARIO para que funcione la importación actual
+export default ProductoData 
